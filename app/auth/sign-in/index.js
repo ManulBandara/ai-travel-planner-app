4dc7addc2106f96d082165d9c418e9d843a04c4b
Manul Bandara <manulbandara@gmail.com>
2024-09-15 17:39:25 +0530

    create sign up and craete acc pages

--- app/auth/sign-in/index.js
+++ app/auth/sign-in/index.js
@@ -1,10 +1,12 @@
-import { View, Text, TextInput, StyleSheet } from 'react-native'
+import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native'
 import React, { useEffect } from 'react'
-import { useNavigation } from 'expo-router'
+import { useNavigation, useRouter } from 'expo-router'
 import { Colors } from './../../../constants/Colors';
+import { useRouteNode } from 'expo-router/build/Route';
 
 export default function SignIn() {
   const navigation=useNavigation();
+  const router=useRouter();
   
   useEffect(()=>{
     navigation.setOptions({
@@ -47,6 +49,46 @@ export default function SignIn() {
           style={styles.input}
         placeholder='Enter Email' />
      </View>
+     <View style={{
+        marginTop: 20
+     }}>
+        <Text style={{
+          fontFamily: "outfit",
+        }}>Password</Text>
+        <TextInput 
+          secureTextEntry={true}
+          style={styles.input}
+        placeholder='Enter Password' />
+     </View>
+
+     <View style={{
+      padding:20,
+      backgroundColor: Colors.PRIMARY,
+      borderRadius: 15,
+      marginTop: 50
+     }}>
+          <Text style={{
+            color: Colors.WHITE,
+            textAlign: 'center',
+          }}>Sign In</Text>
+     </View>
+
+     <TouchableOpacity 
+      onPress={()=> router.replace('auth/sign-up')}
+       
+      style={{
+      padding:20,
+      backgroundColor: Colors.WHITE,
+      borderRadius: 15,
+      marginTop: 20,
+      borderWidth: 1,
+     }}>
+          <Text style={{
+            color: Colors.PRIMARY,
+            textAlign: 'center',
+          }}>Create Account</Text>
+     </TouchableOpacity>
+
     </View>
   )
 }
