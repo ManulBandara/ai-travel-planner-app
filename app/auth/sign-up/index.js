5aa9764e6e33fd42bd3b5f519cb9b82709bf5a4a
Manul Bandara <manulbandara@gmail.com>
2024-09-15 17:54:25 +0530

    create new page with email and passsword

--- app/auth/sign-up/index.js
+++ app/auth/sign-up/index.js
@@ -1,10 +1,58 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, StyleSheet, TouchableOpacity} from 'react-native'
+import React, { useEffect } from 'react'
+import { Colors } from './../../../constants/Colors';
+import { useNavigation } from 'expo-router'
 
 export default function SignUp() {
+  const navigation=useNavigation();
+
+  useEffect(()=>{
+    navigation.setOptions({
+      headerShown: false
+    })
+  },[])
   return (
-    <View>
-      <Text>SignUp</Text>
+    <View style={{
+      padding: 25,
+      paddingTop: 50,
+    
+    }}>
+      <Text style={{
+        fontFamily: "outfit-bold",
+        fontSize: 30,
+      }}>Create New Account</Text>
+
+      <View style={{
+        marginTop: 50
+     }}>
+        <Text style={{
+          fontFamily: "outfit",
+        }}>Email</Text>
+        <TextInput 
+          style={styles.input}
+        placeholder='Enter Email' />
+     </View>
+     <View style={{
+        marginTop: 20
+     }}>
+        <Text style={{
+          fontFamily: "outfit",
+        }}>Password</Text>
+        <TextInput 
+          secureTextEntry={true}
+          style={styles.input}
+        placeholder='Enter Password' />
+     </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  input: {
+    padding: 15,
+    borderWidth: 1,
+    borderColor: Colors.GRAY,
+    borderRadius: 15,
+    fontFamily: "outfit",
+  },
+})
